f2cb0058f58c9adbed6d3f6a2aaf860ad708f1f9
Miriam Chen <45667250+Miriam410@users.noreply.github.com>
2021-03-20 17:35:25 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingreso = "";
+            Console.WriteLine("Por favor, ingrese un texto cualquiera.");
+            ingreso = Console.ReadLine();
+            Console.WriteLine("El texto ingresado es:" + ingreso);
+        }
+    }
+}
